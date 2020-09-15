b50c4806ae5225bfae5824517230ee120741ce86
Marin ROUX <Marin@MacBook-Pro-de-Marin.local>
2020-09-15 19:29:44 +0200

    feat: Added notification when saving plan-prise or settings

--- resources/react-app/src/store/middleware/save-data/index.ts
+++ resources/react-app/src/store/middleware/save-data/index.ts
@@ -2,38 +2,79 @@ import { debounce, get } from 'lodash';
 import axios from 'helpers/axios-clients';
 import { MiddlewareAPI, Dispatch, Action } from 'redux';
 import { setSettings, removeValue, setValue } from 'store/plan-prise';
+import { addNotification, removeNotification } from 'store/app';
 
-const update = debounce((id: number, data: { action: string; value: any }) => {
-  const url = `/plan-prise/${id}`;
-  return axios
-    .patch(url, data, { withCredentials: true })
-    .then((response) => {
-      if (!(response.status === 200)) throw new Error(response.statusText);
-      return true;
-    })
-    .catch((error) => {
-      console.log(error);
-      return false;
-    });
-}, 5000);
+const update = debounce(
+  (
+    id: number,
+    data: { action: string; value: any },
+    onStart?: () => void,
+    callback?: () => void
+  ) => {
+    const url = `/plan-prise/${id}`;
+    onStart && onStart();
+    return axios
+      .patch(url, data, { withCredentials: true })
+      .then((response) => {
+        if (!(response.status === 200)) throw new Error(response.statusText);
+        callback && callback();
+        return true;
+      })
+      .catch((error) => {
+        console.log(error);
+        callback && callback();
+        return false;
+      });
+  },
+  5000
+);
 
-const saveToAPI = (store: MiddlewareAPI<Dispatch, ReduxState>) => (
-  next: Dispatch
-) => (action: Action) => {
-  next(action);
-  if (action.type === removeValue.type || action.type === setValue.type) {
-    const state = store.getState();
-    update(get(state, 'planPrise.id'), {
-      action: 'edit',
-      value: get(state, 'planPrise.content.custom_data'),
-    });
+const allowedActions = [removeValue.type, setValue.type, setSettings.type];
+const SAVING_NOTIFICATION_TYPE = 'saving';
+
+const switchAction = (type: string) => {
+  switch (type) {
+    case removeValue.type:
+    case setValue.type:
+      return { action: 'edit', path: 'planPrise.content.custom_data' };
+    case setSettings.type:
+      return { action: 'settings', path: 'planPrise.content.custom_settings' };
+    default:
+      throw new Error('No action type provided');
   }
-  if (action.type === setSettings.type) {
-    const state = store.getState();
-    update(get(state, 'planPrise.id'), {
-      action: 'settings',
-      value: get(state, 'planPrise.content.custom_settings'),
-    });
+};
+
+const saveToAPI = ({
+  dispatch,
+  getState,
+}: MiddlewareAPI<Dispatch, ReduxState>) => (next: Dispatch) => (
+  action: Action
+) => {
+  next(action);
+  if (allowedActions.includes(action.type)) {
+    const state = getState();
+    const parameters = switchAction(action.type);
+
+    update(
+      get(state, 'planPrise.id'),
+      {
+        action: parameters.action,
+        value: get(state, parameters.path),
+      },
+      () =>
+        dispatch(
+          addNotification({
+            id: SAVING_NOTIFICATION_TYPE,
+            header: 'Sauvegarde en cours...',
+            icon: 'spinner',
+          })
+        ),
+      () => dispatch(removeNotification(SAVING_NOTIFICATION_TYPE))
+    );
+
+    if (action.type === setSettings.type) {
+      update.flush();
+    }
   }
 };
 
