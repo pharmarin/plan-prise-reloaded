65f193223030bba4c7093395f572ab120a4d850f
Marin ROUX <marin@Mac-mini-de-Marin.local>
2020-11-25 19:23:30 +0100

    🐛 FIX: Card take full width

--- resources/react-app/src/base-components/Card/index.tsx
+++ resources/react-app/src/base-components/Card/index.tsx
@@ -5,7 +5,7 @@ const Card: React.FC<{ className?: string }> = ({ children, className }) => {
   return (
     <div
       className={classNames(
-        'w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg',
+        'w-full mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg',
         className
       )}
     >
