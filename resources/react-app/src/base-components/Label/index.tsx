fd473c97cb87279f3452d9137d80e2eac7f2b975
Marin ROUX <marin@Mac-mini-de-Marin.local>
2020-11-27 10:29:46 +0100

    🐛 FIX: Add className prop to Label

--- resources/react-app/src/base-components/Label/index.tsx
+++ resources/react-app/src/base-components/Label/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import classNames from 'classnames';
 
-const Label: React.FC<{ check?: boolean; for?: string }> = ({
-  check,
-  children,
-  for: htmlFor,
-}) => {
+const Label: React.FC<{
+  check?: boolean;
+  className?: string;
+  for?: string;
+}> = ({ check, children, className, for: htmlFor }) => {
   return (
     <label
-      className={classNames('block font-medium text-sm text-gray-700', {
-        'ml-3': check,
-      })}
+      className={classNames(
+        'block font-medium text-sm text-gray-700',
+        {
+          'ml-3': check,
+        },
+        className
+      )}
       htmlFor={htmlFor}
     >
       {children}
