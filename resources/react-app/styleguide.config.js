2fc42f43845d27b76231d0b038c10d22077bad39
Marin ROUX <marin_maxfresh-and-good@hotmail.fr>
2020-11-30 21:44:50 +0100

    📦 NEW: Add styleguidist

--- resources/react-app/styleguide.config.js
+++ resources/react-app/styleguide.config.js
@@ -3,5 +3,8 @@ const path = require('path');
 module.exports = {
   components: 'src/base-components/**/*.tsx',
   defaultExample: true,
+  propsParser: require('react-docgen-typescript').withCustomConfig(
+    './tsconfig.json'
+  ).parse,
   require: [path.join(__dirname, 'src/styles.css')],
 };
