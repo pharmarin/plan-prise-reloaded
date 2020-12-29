295e0f3a113c71abcc71ed2a2fc5362673e3ce88
Marin ROUX <marin@Mac-mini-de-Marin.local>
2020-12-29 19:10:30 +0100

    📦 NEW: Use axios in datx

--- resources/react-app/src/store/json-api.ts
+++ resources/react-app/src/store/json-api.ts
@@ -1,12 +1,26 @@
+import { AxiosRequestConfig } from 'axios';
 import { Collection } from 'datx';
 import { config, jsonapi } from 'datx-jsonapi';
+import axios from 'helpers/axios-clients';
 import ApiMedicament from 'models/ApiMedicament';
 import Medicament from 'models/Medicament';
 import Precaution from 'models/Precaution';
 import PrincipeActif from 'models/PrincipeActif';
 import User from 'models/User';
 
-config.baseUrl = 'http://localhost:3000/api/v1/';
+config.baseFetch = (method, url, body, requestHeaders, fetchOptions) =>
+  axios
+    .request({
+      method: method as AxiosRequestConfig['method'],
+      url,
+      data: body,
+    })
+    .then((response) => ({
+      data: response.data,
+      status: response.status,
+      requestHeaders,
+      headers: new Headers(response.headers),
+    }));
 
 class JsonApiStore extends jsonapi(Collection) {
   static types = [ApiMedicament, Medicament, Precaution, PrincipeActif, User];
