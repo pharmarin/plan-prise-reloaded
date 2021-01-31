41b2a95c526683ab7fb9e1f103d2a2c39f8ed5bf
Marin ROUX <marin@Mac-mini-de-Marin.local>
2021-01-31 10:22:26 +0100

    📦 NEW: Add notification.addNotification helper

--- resources/react-app/src/store/notification-bag.ts
+++ resources/react-app/src/store/notification-bag.ts
@@ -3,6 +3,17 @@ import Notification from 'models/Notification';
 
 class NotificationBag extends Collection {
   static types = [Notification];
+
+  addNotification(notification: {
+    title: string;
+    message?: string;
+    timer?: number;
+    type?: string;
+  }) {
+    const notificationRef = new Notification(notification);
+    this.add(notificationRef);
+    return notificationRef;
+  }
 }
 
 export default NotificationBag;
